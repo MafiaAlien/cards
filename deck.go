ccd23dc33f21abec449a14d137f49574f3e9c75a
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2022-09-15 13:34:57 -0700

    second commit

--- deck.go
+++ deck.go
@@ -1,2 +1,32 @@
-package main 
+package main
 
+import "fmt"
+
+// create a new type of deck
+// which is a slice of strings
+
+type Deck []string
+
+func (d Deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+
+func newDeck() Deck {
+	cards := Deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+
+}
+
+func (d Deck) shuffle() {
+
+}
