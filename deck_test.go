ed778440c1d25080326a0c4efa2266c657fb9ae8
MafiaAlien <49702823+MafiaAlien@users.noreply.github.com>
2022-09-16 21:22:21 -0700

    add testing file

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,30 @@
-package main 
+package main
 
 import (
+	"os"
 	"testing"
 )
 
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
 
-func TestNewDeck(t *testing.T){
-	cards := newDeck()
-	cards.print()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+}
+
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+	
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	
+	d_copy := newDeckFromFile("_decktesting")
+	if len(d_copy) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(d_copy))
+	}
+
+	os.Remove("_decktesting")
 }
